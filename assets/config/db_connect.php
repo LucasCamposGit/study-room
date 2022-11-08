e30fe3c72d2a2ba44dbf0fd8c4ed82d2865ed54a
LucasCamposGit <lucascpn2501@gmail.com>
2022-11-08 12:18:54 -0300

    pdo

--- assets/config/db_connect.php
+++ assets/config/db_connect.php
@@ -1,8 +1,16 @@
 <?php 
+    $dsn = 'mysql:host=localhost;dbname=flashcards';
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_BOTH,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
 
     try{
-        $db = new PDO('mysql:host=localhost;dbname=flashcards', 'lucas', 'lu250101');
+        $db = new PDO($dsn, 'lucas', 'lu250101', $options);
+
     } catch (\PDOException $e){
-        throw new \PDOException($e->getMessage(), $e->getCode());
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
     }
 ?>
